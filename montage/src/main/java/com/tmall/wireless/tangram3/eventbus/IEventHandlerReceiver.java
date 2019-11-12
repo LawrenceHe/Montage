b9daba4ac408b8b3d76b2e352f63e26c3e81b4a2
helun <helun@Ctrip.com>
2019-11-12 16:33:54 +0800

    去掉tangram3,Login页面迁移至App模块

--- montage/src/main/java/com/tmall/wireless/tangram3/eventbus/IEventHandlerReceiver.java
+++
@@ -1,5 +0,0 @@
-package com.tmall.wireless.tangram3.eventbus;
-
-public interface IEventHandlerReceiver {
-    void handleEvent(Event event);
-}
--- montage/src/main/java/com/tmall/wireless/tangram3/support/TemplateUpdateSupport.java
+++
@@ -1,9 +0,0 @@
-package com.tmall.wireless.tangram3.support;
-
-import com.tmall.wireless.tangram3.structure.TemplateInfo;
-
-public class TemplateUpdateSupport {
-    public void onUpdate(TemplateInfo templateInfo) {
-
-    }
-}
